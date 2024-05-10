e91d1cfd8e3f935be26813ae1a11750103c64128
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2024-05-11 04:25:27 +0530

    syncing

--- server.js
+++ server.js
@@ -26,4 +26,8 @@ app.post("/translate", (req, res) => {
     res.send(ans);
   });
 });
-app.listen(4000);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
