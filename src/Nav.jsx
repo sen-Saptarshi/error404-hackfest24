95565d243948bf4f99b4d43483fb1f4df337049c
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2024-05-10 20:49:52 +0530

    nav1

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
