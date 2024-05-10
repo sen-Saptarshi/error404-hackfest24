a22e66d24436db380a7139ed93a54fa38211ca6f
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2024-05-10 21:16:29 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Nav from "./Nav";
+import Input from "./Input";
+import Output from "./Output";
 function App() {
   return (
     <>
       <h1>TransLingoGPT</h1>
       <Nav/>
+      <Input/>
+      <Output/>
     </>
   );
 }
